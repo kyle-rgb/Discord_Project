438c41d9a5aac1ac186cd66db8544ae4304cf2ad
Kyle Lewis <kylel9815@gmail.com>
2022-01-21 16:22:10 -0500

    Added Portfolio Based on Mention Frequency and SPY Benchmark to Vue Charts

--- src/static/js/apexdataprep.js
+++ src/static/js/apexdataprep.js
@@ -1,17 +1,56 @@
-function createPortfolio(data, start_date=new Date('2020-01-01'), end_date=new Date('2021-01-01')){
+// function based off of stocks 
+function createPortfolio(data, start_date=new Date('2020-01-01 00:00:00'), end_date=new Date('2021-01-01 00:00:00')){
     // bring in comment data & aggregate 
     var mentions_total = c_data.map((c) => {return c.counts}).reduce((m, n) => {return m+=n})
     var assets = c_data
     //port.sort((a, b) => new Date(a.Date) - new Date(b.Date))
     for (let i=0; i < assets.length; i++){
-        assets[i]['data'] = port.filter((c) => {return c.symbol == assets[i].symbol}).filter((c) => {return ((new Date(c.Date) >= start_date)&( new Date(c.Date)<= end_date))})
-        assets[i].start_date = assets[i].data[0].Date.split(' ')[0]
+        assets[i]['data'] = data.filter((c) => {return c.symbol == assets[i].symbol}).filter((c) => {return ((new Date(c.Date) >= start_date)&( new Date(c.Date)<= end_date))})
+        assets[i].start_date = assets[i].data[0].Date
     }
     // use the preference dates to grab starting close line for specific stock
     assets.sort((a, b) => {return new Date(a.start_date) - new Date(b.start_date)})
     var grouped = d3.groups(assets, d => d.start_date)
     var l = grouped.map((c) => {return c[1].map(function(x) { return x.counts}).reduce((m, n) => {return m+=n})})
-    console.log(l)
-    console.log(grouped)
-    return grouped
+    var j = 0;
+    l = l.map((a) => {j+=a; return j})
+    d =  grouped.map((d, i) => d[0])
+    // v = d.reduce((obj, k, i) => ({...obj, [k]: l[i] }), {})
+    // console.log(v)
+    // with grouped it calculates portfolio based on date and number; calc share price based on total mentions of stock by available data;
+    var f = grouped.map((g) => {
+        return g[1].map((stock) => {
+            var index = 0;
+            var total = 0;
+            return stock.data.map((f) => {
+                _date = new Date (f.Date)
+                if (_date < new Date(d[index])){
+                    f.port_weight = stock.counts / l[index]
+                    f.mention_price = f.port_weight * f.Close
+                    return {date: _date,  asset_price: f.mention_price, symbol: f.symbol}
+                } else {
+                    index++
+                    index > l.length-1 ? index =l.length-1 : index=index;
+                    f.port_weight = (stock.counts / l[index])
+                    f.mention_price = f.port_weight * f.Close
+                    return {date: _date,  asset_price: f.mention_price, symbol: f.symbol}
+                }
+            })
+        })
+    }).map((z) => {return z.reduce((u, u1) => {return u.concat(u1)})}).reduce((r, r1) => {return r.concat(r1)})
+    // rollup
+    console.log(f)
+    var rollup = d3.rollup(f, v => d3.sum(v, d => d.asset_price), d=>d.date)
+    var ff = [];
+    for (e of rollup.entries()){
+        ff.push(e)
+    }
+    return ff.map((b) => [b[0].getTime(), +(b[1].toFixed(2))])
+}
+
+
+
+function norm(chart_data){
+    start = chart_data[0][1]
+    return (chart_data.map((r) => {return [r[0], r[1] / start]}))
 }
--- src/static/js/portfolio.js
+++ src/static/js/portfolio.js
@@ -1,6 +1,6 @@
 (function() {
     // computed: object of run functions / watch: progress f(x) / created: set atrributes and run functions on app creation (use this) / methods to run. @click
-    var d_ = port.filter((m)=> m.symbol=='PLTR').map((m)=> [new Date(m.Date).getTime(), m.Close])
+    var d_ = norm(createPortfolio(port))//  
     
 	var svgNS = 'http://www.w3.org/2000/svg';
 	var app = new Vue({
@@ -19,8 +19,13 @@
                 options: {
                     series: [{
                         data: d_,
-                        name: "PLTR"
-                    }],
+                        name: "Portfolio Based on Mentions"
+                    },
+                    {
+                        data: norm(port.filter(function (m) {return ((m.symbol =='SPY')&(new Date(m.Date) >= new Date('2020-01-01 00:00:00'))&(new Date(m.Date) < new Date('2021-01-01 00:00:00')))}).map((f) => {return [new Date(f.Date).getTime(), f.Close]})),
+                        name: 'SP 500 Index'
+                    },
+                ],
                     dataLabels: {
                         enabled: false
                       },
@@ -33,14 +38,21 @@
                         labels: {style: {colors: 'white'} }
                       },
                     yaxis: {
-                        labels: {style: {colors: 'white'} }
+                        tickAmount: 5,
+                        labels: {style: {colors: 'white'}, formatter: (val) => { return val.toFixed(4) } }
                       },
                     tooltip: {
                         x: {
                           format: 'dd MMM yyyy'
-                        }
+                        },
+                        y: {
+                            formatter: function(value, { series, seriesIndex, dataPointIndex, w }) {
+                                return (((value / series[seriesIndex][0]) - 1)*100).toFixed(2) + "%" 
+                              }
+                        },
                         },
                     fill: {
+                        colors: ['purple', 'green'],
                         type: 'gradient',
                         gradient: {
                           shadeIntensity: 1,
@@ -49,6 +61,16 @@
                           stops: [0, 150]
                         }
                       },
+                    legend: {
+                        labels: {
+                            colors: ['white']
+                        },
+                        position: 'top',
+                        markers: {
+                            width: 30,
+                            height: 30,
+                        },
+                    }
                 },
                 
                 
